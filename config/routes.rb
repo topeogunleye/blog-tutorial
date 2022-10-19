16c003d345b6f960cee977ad44ad385bbdefd1b0
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-10-19 20:14:03 +0100

    Add routes to articles#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "articles/:id", to: "articles#show"
   get "/articles", to: "articles#index"
   root "articles#index"
   # Defines the root path route ("/")
