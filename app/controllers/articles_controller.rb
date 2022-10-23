3e55be0823b67716f3a4932d46a92321026cd5d4
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-10-23 22:36:33 +0100

    Reach Lesson 18 - Resources and Routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,47 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
+  end
+
+  def new
+    @article = Article.new
+  end
+  
+  def create 
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to articles_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(articles_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
     @article = Article.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,31 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "articles/:id", to: "articles#show"
-  get "/articles", to: "articles#index"
-  root "articles#index"
+  # root "articles#index"
+
+  # # Routes for updating articles
+  # patch "/articles/:id", to: "articles/update"
+
+  # # Routes for editing articles
+  # get "/articles/:id/edit", to: "articles#edit"
+
+  # # Routes for creating articles
+  # post "/articles", to: "articles#create"
+
+  # # Routes for new article form
+  # get "/articles/new", to: "articles#new"
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show", as: :article
   # Defines the root path route ("/")
   # root "articles#index"
+
+  delete "/articles/:id", to: "articles#destroy"
+  patch "/articles/:id", to: "articles#update"
+  get "/articles/:id/edit", to: "articles#edit"
+  post "/articles", to: "articles#create"
+  get "/articles/new", to: "articles#new"
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show", as: :article
+  # Defines the root path route ("/")
+  root "articles#index"
 end
